54435467b3f11601f7c2d44d0fdd3e32f92c1db3
pdor <pdor92@hotmail.it>
2022-03-11 00:01:36 +0100

    Created Game object and render function.

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 "use strict";
+
+const Game = {
+    board: new Array(9),
+
+    render: function () {
+        const container = document.querySelector(".board-container");
+        const cells = container.children;
+        for (let i = 0; i < 9; i++) {
+            cells.item(i).innerText = this.board[i];
+        }
+    },
+};
+
+function Player (symbol) {
+    const proto = {
+        playMove() {
+            
+        }
+    }
+    return {symbol}
+}
+
+Game.board = ["O", "X", " ",
+              " ", "O", "X",
+              " ", "O", "X"];
+
+Game.render();
